8bf2f0108c013f7465551587d468ff2e3003bf81
Vansh More <vanshmore08@gmail.com>
2026-02-19 20:06:57 +0530

    popup add

--- Backend/server.js
+++ Backend/server.js
@@ -31,3 +31,4 @@ mongoose.connect(process.env.MONGO_URI)
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
